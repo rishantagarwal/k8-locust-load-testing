bd65cb18de730703f04b485ca40d9c918526286a
Rishant Agarwal <bindasrishant@gmail.com>
2019-08-30 15:30:48 +0900

    chore: setup for knative

--- locust-tasks/buildAndPushDocker.sh
+++ locust-tasks/buildAndPushDocker.sh
@@ -1,1 +1,2 @@
-docker build --tag=locust-tasks --tag=asia.gcr.io/tegaki-benchmark-prio-queue/locust-tasks . && docker push asia.gcr.io/tegaki-benchmark-prio-queue/locust-tasks
+# docker build --tag=locust-tasks --tag=asia.gcr.io/tegaki-benchmark-prio-queue/locust-tasks . && docker push asia.gcr.io/tegaki-benchmark-prio-queue/locust-tasks
+docker build --tag=locust-tasks --tag=rishantagarwal/locust-tasks . && docker push rishantagarwal/locust-tasks
--- nginx-rate-limit/buildAndPushDocker.sh
+++ nginx-rate-limit/buildAndPushDocker.sh
@@ -1,1 +1,2 @@
-docker build --tag=nginx-rate-limiting-testing --tag=asia.gcr.io/tegaki-benchmark-prio-queue/nginx-rate-limiting-testing . && docker push asia.gcr.io/tegaki-benchmark-prio-queue/nginx-rate-limiting-testing
+# docker build --tag=nginx-rate-limiting-testing --tag=asia.gcr.io/tegaki-benchmark-prio-queue/nginx-rate-limiting-testing . && docker push asia.gcr.io/tegaki-benchmark-prio-queue/nginx-rate-limiting-testing
+docker build --tag=nginx-rate-limiting-testing --tag=rishantagarwal/nginx-rate-limiting-testing . && docker push rishantagarwal/nginx-rate-limiting-testing
